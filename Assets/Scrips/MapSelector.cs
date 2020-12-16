ddb51cec5d56940812b696c888393df3b69b54fa
cristiancito1 <thebigpeterg@gmail.com>
2020-12-16 13:43:41 +0100

    Seleccion Pronsajes

---
+++ Assets/Scrips/MapSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapSelector : MonoBehaviour
+{
+    public void Map1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Map2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scrips/PlayerSelector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PlayerSelector : MonoBehaviour
+{
+    public int firstchar;
+    public int secondChar;
+    public Sprite[] playerSprites;
+    public SpriteRenderer player1;
+    public SpriteRenderer player2;
+
+    public void selectCampeon(int numero)
+    {
+        firstchar = numero;   
+    }
+    public void selectcampeon2(int numero)
+    {
+        secondChar= numero;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Jugar()
+    {
+        player1.sprite = playerSprites[firstchar];
+        player2.sprite = playerSprites[secondChar];
+    }
+    public void ChargeMapScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
