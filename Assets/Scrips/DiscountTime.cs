bb3fc636f06badb7de436602ad71c4120d51861d
cristiancito1 <thebigpeterg@gmail.com>
2020-12-17 10:42:02 +0100

    TiempoCdMenos

---
+++ Assets/Scrips/DiscountTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DiscountTime : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+
+
+    [SerializeField] Text countdownText;
+
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString();
+
+        
+    }
+}
+
+
